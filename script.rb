faaa4a1ffa7c99e2d77af62f76fed4f1b34858be
datrolld20 <datrolld20@gmail.com>
2020-09-24 13:33:04 -0400

    Got rid of debugging prints

--- script.rb
+++ script.rb
@@ -3,13 +3,10 @@ def stock_picker(arr)
     arr.index(pair[0]) < arr.index(pair[1])
   end
 
-  p day_pairs
-
   result = day_pairs.reduce([1,1]) do |result, pair|
-    p "Compare" + result.to_s
-    p "to" + pair.to_s
     (pair[1] / pair[0]) > (result[1] / result[0]) ? pair : result
   end
+  
   result
 end
 
